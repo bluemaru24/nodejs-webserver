5710c47c20e0a3052d9ea74ea7e0f489c7955256
Randy Dwi Romadhoni <randy.romadhoni@gmail.com>
2024-02-28 14:18:58 +0700

    belajar url dan method

--- server.js
+++ server.js
@@ -2,14 +2,59 @@ const http = require('http');
  
 const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
- 
     response.statusCode = 200;
-    response.end('<h1>Halo HTTP Server!</h1>');
+ 
+    const { url, method } = request;
+ 
+    if(url === '/') {
+        if(method === 'GET'){
+            response.end('<h1>Ini adalah homepage</h1>');
+        }
+        else{
+            response.end('<h1>Halaman tidak dapat diakses dengan <any> request</h1>');
+        }
+    }
+    else if(url === '/about'){
+        if(method === 'GET'){
+            response.end('<h1>Halo! Ini adalah halaman about</h1>');
+        }
+        else if(method === 'POST'){
+            let body = [];
+    
+            request.on('data', (chunk) => {
+                body.push(chunk);
+            });
+        
+            request.on('end', () => {
+                body = Buffer.concat(body).toString();
+                const { name } = JSON.parse(body);
+                response.end(`<h1>“Halo,  ${name}! Ini adalah halaman about!</h1>`);
+            });
+        }
+        else {
+            response.end('<h1>Halaman tidak dapat diakses dengan <any> request</h1>');
+        }
+    }
+    else{
+        response.end('<h1>Halaman tidak ditemukan!</h1>');
+    }
+ 
+    // if(method === 'POST') {
+    //     let body = [];
+    
+    //     request.on('data', (chunk) => {
+    //         body.push(chunk);
+    //     });
+       
+    //     request.on('end', () => {
+    //         body = Buffer.concat(body).toString();
+    //         const { name } = JSON.parse(body);
+    //         response.end(`<h1>Hai, ${name}!</h1>`);
+    //     });
+    // }
 };
  
- 
 const server = http.createServer(requestListener);
- 
 const port = 5000;
 const host = 'localhost';
  
